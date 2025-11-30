1392c63bffd84921ebc1d44ea49a499c1c0aef49
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-30 23:25:36 +0545

    finally compeleted the transaction history

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
-import {transaction} from '../store/data.js'
+import { transaction } from '../store/data.js'
+import { useRecoilState } from 'recoil'
+import { StatusAtom } from '../store/atoms/main.js'
 
 export default function Transactions() {
+  // const [color, setColor] = useRecoilState(StatusAtom)
   console.log(transaction);
-    const heading = ["Order ID", "Status", "Transaction ID", "Refund Date", "Order Amount"]
-    
+  const heading = ["Order ID", "Status", "Transaction ID", "Refund Date", "Order Amount"]
+
   return (
     <div>
-        <div id="heading"
-        className='flex justify-between items-center py-2 px-2 bg-gray-200 mt-5'
+      <div className="grid grid-cols-5 gap-5 py-2 px-2 bg-gray-200 mt-5">
+        {heading.map((title, idx) => (
+          <div key={idx}
+          className={idx === heading.length - 1 ? "justify-self-end" : ""}
+          >{title}</div>
+        ))}
+      </div>
+
+      {transaction.map((details, idx)=> (
+        <div
+          key={idx}
+          className="grid grid-cols-5 gap-5 py-2 px-2"
         >
-          {heading.map((title, idx)=>{
-            return <div key={idx}>
-              {title}
-            </div>
-          })}
+          <div className='text-blue-700'>#{details.OrderId}</div>
+          {details.Status === "Processing" && <div className='flex items-center gap-1'>
+            <div className='w-2.5 h-2.5 rounded-full bg-gray-400'></div>
+            {details.Status}
+          </div>}
+          {details.Status === "Successfull" && <div className='flex items-center gap-1'>
+            <div className='w-2.5 h-2.5 rounded-full bg-green-600'></div>
+            {details.Status}
+          </div>}
+
+          <div>{details.TransactionId}</div>
+          <div>{details.RefundDate}</div>
+          <div className='justify-self-end'>{details.OrderAmount}</div>
         </div>
+      ))}
+
     </div>
   )
 }
--- src/store/data.js
+++ src/store/data.js
@@ -2,54 +2,54 @@ export const transaction =[{
     OrderId : "2009",
     Status : "Successfull",
     TransactionId : "TRX009",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },{
     OrderId : "2010",
     Status : "Processing",
     TransactionId : "TRX010",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "100"
 },{
     OrderId : "2011",
     Status : "Successfull",
     TransactionId : "TRX011",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },{
     OrderId : "2012",
     Status : "Successfull",
     TransactionId : "TRX012",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },{
     OrderId : "2009",
     Status : "Processing",
     TransactionId : "TRX009",
-    RefunDate : "Yesterday, 1:30 PM",
+    RefundDate : "Yesterday, 1:30 PM",
     OrderAmount : "500"
 },{
     OrderId : "2009",
     Status : "Successfull",
     TransactionId : "TRX009",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },{
     OrderId : "2009",
     Status : "Processing",
     TransactionId : "TRX009",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },{
     OrderId : "2009",
     Status : "Processing",
     TransactionId : "TRX009",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },{
     OrderId : "2009",
     Status : "Successfull",
     TransactionId : "TRX009",
-    RefunDate : "Today, 4:30 PM",
+    RefundDate : "Today, 4:30 PM",
     OrderAmount : "12500"
 },]
